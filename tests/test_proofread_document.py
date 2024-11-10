71df44eda9e0bb9000a50da8ae599a63df6a2198
caerulex <timelesswind@gmail.com>
2024-11-10 01:15:36 +0100

    Improved test case.

--- tests/test_proofread_document.py
+++ tests/test_proofread_document.py
@@ -19,7 +19,9 @@ def test_proofread_short_document():
 def test_proofread_short_document_additional_instructions():
     test_doc = Path(TEST_RESOURCE_DIR, "test_doc.docx")
     outputs = proofread_document(
-        test_doc, additional_instructions="Make sure you catch all the typos! Like mistake vs mistak", save_outputs=False
+        test_doc,
+        additional_instructions="'mistak' is a valid word in this document; do not correct it.",
+        save_outputs=False,
     )
-    assert "mistake" in outputs
+    assert "mistake" not in outputs
     assert "leafy" in outputs
